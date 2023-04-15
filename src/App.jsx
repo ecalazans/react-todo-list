51122a0b956f2010030fe5781630a3f680442f96
laryjsm <laryssajsm@gmail.com>
2023-04-15 09:57:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+
+import { Container, ToDoList, Input, Button, ListItem, Trash, Check, H3 } from "./styles.js";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [inputTask, setInputTask] = useState("");
+
+  function inputMudou(event) {
+    setInputTask(event.target.value);
+  }
+
+  function cliqueiNoBotao() {
+    if (inputTask) {
+    setList([...list, { id: uuid(), task: inputTask, finished: false }]);
+    setInputTask("")
+  }
+}
+
+  function finalizarTarefa(id) {
+    const newList = list.map((item) =>
+      item.id === id ? { ...item, finished: !item.finished } : item
+    );
+
+    setList(newList);
+  }
+
+  function deletarItem(id) {
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
+  }
+
+  return (
+    <Container>
+      <ToDoList>
+        <Input onChange={inputMudou} placeholder="O que tenho que fazer..." value={inputTas} />
+        <Button onClick={cliqueiNoBotao}>Adicionar</Button>
+
+        <ul>
+          {list.length > 0 ? (
+          list.map((item) => (
+            <ListItem isFinished={item.finished} key={item.id}>
+              <Check onClick={() => finalizarTarefa(item.id)} />
+              <li>{item.task}</li>
+              <Trash onClick={() => deletarItem(item.id)} />
+            </ListItem>
+          ))
+          ) : (
+            <H3>Não há itens na lista</H3>
+          )}
+        </ul>
+      </ToDoList>
+    </Container>
+  );
+}
+
+export default App;
